80ffaf6f464e0ef2b5f461ef67788e498039f95d
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-08 15:01:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
