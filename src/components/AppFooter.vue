6e5ef42af665d91ed2215dc5252dcc963536789f
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-08 17:01:00 +0100

    feat: add footer component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,81 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      footerLink: [
+        {
+          text: "Informazioni legali",
+          href: "#",
+        },
+        {
+          text: "Informativa sulla privacy",
+          href: "#",
+        },
+        {
+          text: "Diritto di recesso",
+          href: "#",
+        },
+      ],
+
+      footerIcons: [
+        "fa-brands fa-square-twitter",
+        "fa-brands fa-square-facebook",
+        "fa-brands fa-square-instagram",
+        "fa-brands fa-square-pinterest",
+        "fa-brands fa-square-youtube",
+      ],
+    };
   },
 };
 </script>
 
 <template>
-  <footer>app footer</footer>
+  <footer>
+    <div class="container">
+      <div class="footer-left">
+        <h3>Boolando s.r.l</h3>
+        <ul>
+          <li v-for="link in footerLink">
+            <a :href="link.href">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer-right">
+        <p>Trovaci anche su</p>
+        <ul>
+          <li v-for="icon in footerIcons">
+            <a href="#">
+              <i :class="icon" class="fa-2x"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+  padding: 2rem 0;
+  background-color: #1a1a1a;
+  color: white;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    ul {
+      display: flex;
+      gap: 10px;
+      font-size: 0.7rem;
+      margin-top: 1rem;
+    }
+    li {
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
