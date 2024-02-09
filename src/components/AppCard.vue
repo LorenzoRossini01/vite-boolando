228425bd5ea5de88d9c69e5a8985e48985d3e698
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-09 11:45:44 +0100

    feat: add AppMain cards as component AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,139 @@
+<script>
+export default {
+  props: {
+    card: Object,
+  },
+
+  methods: {
+    getImgUrl(imgName) {
+      return new URL(`../assets/img/${imgName}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div
+      class="card-img"
+      @mouseover="card.state.hover = true"
+      @mouseleave="card.state.hover = false"
+    >
+      <img :src="getImgUrl(card.sprites.firstImg)" alt="" />
+      <img
+        :src="getImgUrl(card.sprites.secondImg)"
+        :class="card.state.hover ? '' : 'active'"
+        alt=""
+        class="second-img"
+      />
+      <div class="badges">
+        <div v-show="card.bagde.discountValue > 0" class="discount">
+          -{{ card.bagde.discountValue }}%
+        </div>
+        <div v-show="card.bagde.sostenibility" class="sostenibility">
+          Sostenibilità
+        </div>
+      </div>
+
+      <div
+        @click="card.state.favourite = !card.state.favourite"
+        :class="card.state.favourite ? 'active' : ''"
+        class="favourites"
+      >
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </div>
+    <div class="card-details">
+      <p>{{ card.brand }}</p>
+      <h3>{{ card.title }}</h3>
+      <div class="price">
+        <p>
+          {{ card.descountedPrice }} €
+          <span> {{ card.originalPrice }} € </span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: calc(100% / 3 - 10px);
+  margin: 5px;
+  .card-img {
+    position: relative;
+    img {
+      transition: all linear 0.1s;
+      width: 100%;
+      &.second-img {
+        position: absolute;
+        top: 0;
+        &.active {
+          opacity: 0;
+        }
+      }
+    }
+    .badges {
+      position: absolute;
+      bottom: 2rem;
+      display: flex;
+      gap: 5px;
+      div {
+        padding: 3px 7px;
+        background-color: aliceblue;
+        color: white;
+        font-weight: 600;
+        font-size: 0.6rem;
+        &.sostenibility {
+          background-color: green;
+        }
+        &.discount {
+          background-color: red;
+        }
+      }
+    }
+
+    .favourites {
+      width: 50px;
+      height: 50px;
+      background-color: aliceblue;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      right: 0;
+      top: 1rem;
+      cursor: pointer;
+
+      &.active {
+        color: red;
+      }
+    }
+  }
+
+  .card-details {
+    margin: 5px 0;
+    font-size: 0.8rem;
+    p {
+      text-transform: capitalize;
+    }
+
+    h3 {
+      font-size: 1rem;
+      text-transform: uppercase;
+    }
+
+    .price {
+      p {
+        color: red;
+        font-weight: 600;
+      }
+      span {
+        color: black;
+        text-decoration: line-through;
+        font-weight: 200;
+      }
+    }
+  }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,5 @@
 <script>
+import AppCard from "./AppCard.vue";
 export default {
   data() {
     return {
@@ -116,14 +117,8 @@ export default {
     };
   },
 
-  methods: {
-    getImgUrl(imgName) {
-      return new URL(`../assets/img/${imgName}`, import.meta.url).href;
-    },
-
-    getWordCapitalized(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    },
+  components: {
+    AppCard,
   },
 };
 </script>
@@ -131,47 +126,7 @@ export default {
 <template>
   <main>
     <div class="container">
-      <div v-for="card in cards" class="card">
-        <div
-          class="card-img"
-          @mouseover="card.state.hover = true"
-          @mouseleave="card.state.hover = false"
-        >
-          <img :src="getImgUrl(card.sprites.firstImg)" alt="" />
-          <img
-            :src="getImgUrl(card.sprites.secondImg)"
-            :class="card.state.hover ? '' : 'active'"
-            alt=""
-            class="second-img"
-          />
-          <div class="badges">
-            <div v-show="card.bagde.discountValue > 0" class="discount">
-              -{{ card.bagde.discountValue }}%
-            </div>
-            <div v-show="card.bagde.sostenibility" class="sostenibility">
-              Sostenibilità
-            </div>
-          </div>
-
-          <div
-            @click="card.state.favourite = !card.state.favourite"
-            :class="card.state.favourite ? 'active' : ''"
-            class="favourites"
-          >
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </div>
-        <div class="card-details">
-          <p>{{ getWordCapitalized(card.brand) }}</p>
-          <h3>{{ card.title.toUpperCase() }}</h3>
-          <div class="price">
-            <p>
-              {{ card.descountedPrice }} €
-              <span> {{ card.originalPrice }} € </span>
-            </p>
-          </div>
-        </div>
-      </div>
+      <app-card v-for="card in cards" :card="card" />
     </div>
   </main>
 </template>
@@ -184,81 +139,6 @@ main {
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
-    .card {
-      width: calc(100% / 3 - 10px);
-      margin: 5px;
-      .card-img {
-        position: relative;
-        img {
-          transition: all linear 0.1s;
-          width: 100%;
-          &.second-img {
-            position: absolute;
-            top: 0;
-            &.active {
-              opacity: 0;
-            }
-          }
-        }
-        .badges {
-          position: absolute;
-          bottom: 2rem;
-          display: flex;
-          gap: 5px;
-          div {
-            padding: 3px 7px;
-            background-color: aliceblue;
-            color: white;
-            font-weight: 600;
-            font-size: 0.6rem;
-            &.sostenibility {
-              background-color: green;
-            }
-            &.discount {
-              background-color: red;
-            }
-          }
-        }
-
-        .favourites {
-          width: 50px;
-          height: 50px;
-          background-color: aliceblue;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          position: absolute;
-          right: 0;
-          top: 1rem;
-          cursor: pointer;
-
-          &.active {
-            color: red;
-          }
-        }
-      }
-
-      .card-details {
-        margin: 5px 0;
-        font-size: 0.8rem;
-
-        h3 {
-          font-size: 1rem;
-        }
-
-        .price {
-          p {
-            color: red;
-            font-weight: 600;
-          }
-          span {
-            color: black;
-            text-decoration: line-through;
-            font-weight: 200;
-          }
-        }
-      }
-    }
   }
 }
 </style>
